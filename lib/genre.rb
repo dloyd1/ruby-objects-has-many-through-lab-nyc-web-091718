403f545da1d03521dbb41cdd64ff02deeb2b6b4c
Daniel Loyd <dhloyd@icloud.com>
2018-09-21 21:13:40 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,28 +1,26 @@
 class Genre
-attr_accessor :name
-
+  attr_accessor :name
 
   @@all = []
-
-  def self.all
-    @@all
-  end
 
   def initialize(name)
     @name = name
     @@all << self
   end
 
-  def songs
-    Song.all {|song| song.genre == self}
+  def self.all
+    @@all
   end
 
+  def songs
+    Song.all { |song| song.genre == self }
+  end
 
   def artists
     songs.map(&:artist)
   end
 
-def add_song(song)
-  songs << song
-
-end #end of class
+  def add_song(song)
+    songs << song
+  end
+end
