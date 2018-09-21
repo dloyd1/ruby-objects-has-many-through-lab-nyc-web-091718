579f011e8517c1d841f2f3a940bce20a7c7a91de
Daniel Loyd <dhloyd@icloud.com>
2018-09-21 21:13:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,26 @@
-#require 'pry'
 class Artist
   attr_accessor :name
 
   @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
   def self.all
     @@all
   end
 
-def initialize(name)
-  @name = name
-  @@all << self
-end
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
 
-def new_song(name, genre)
-  Song.new(name, self, genre)
-end
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
 
-def songs
-  Song.all.select { |x| x.artist == self}
-end
-
-def genres
-   songs.map(&:genre)
-end
-
-
+  def genres
+    songs.map(&:genre)
+  end
 end
